c251ff62a4a1f8053b51200a0cc0b3a642e0e770
DARK2021 <aladinhabibii@gmail.com>
2019-08-27 15:43:40 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import "./style.css";
+
+function App() {
+  return (
+    <div className="App">
+
+ <div style={{border:' solid 1px black', maxWidth:"100vw"}}>
+
+<h1 className="title red">My title</h1>
+
+<br/>
+
+<img src="/myImage1.png" />
+
+<br/>
+
+<img src="/myImag2.png" />
+
+</div>
+</div>
+     
+     
+    
+   
+    
+  );
+}
+
+export default App;
